f3ff398e9e60728b4cd3d1e40d1b49160d269ec8
Mauricio Barón <diegombaron@gmail.com>
2023-05-26 19:34:04 -0500

    solucion a los ejercicios de js en el navegador

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const input = document.querySelector('#texto');
+const resultado = document.querySelector('#resultado');
+const boton = document.getElementsByClassName('btn')[0];
+const resultadoMayusculas = document.getElementById('resultadoMayusculas');
+
+
+input.addEventListener('input', function() {
+  resultado.textContent = input.value;
+});
+
+ boton.addEventListener('click', function() {
+  const texto = input.value.toUpperCase();
+  resultadoMayusculas.textContent = texto;
+ 
+});
+  
