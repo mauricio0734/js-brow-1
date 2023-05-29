d6643db599a130a089b4849535b561f6acfb49a7
Mauricio Barón <diegombaron@gmail.com>
2023-05-29 10:32:29 -0500

    Ejercicio resuelto

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 
-const input = document.querySelector('#texto');
-const resultado = document.querySelector('#resultado');
-const boton = document.getElementsByClassName('btn')[0];
-const resultadoMayusculas = document.getElementById('resultadoMayusculas');
+const input = document.querySelector("#texto");
+const resultado = document.querySelector("#resultado");
+const boton = document.getElementsByClassName("btn")[0];
+const resultadoMayusculas = document.getElementById("resultadoMayusculas");
 
-
-input.addEventListener('input', function() {
-  resultado.textContent = input.value;
+input.addEventListener("input", function() {
+    resultado.textContent = input.value;
+    
 });
 
- boton.addEventListener('click', function() {
+ boton.addEventListener("click", function() {
   const texto = input.value.toUpperCase();
   resultadoMayusculas.textContent = texto;
  
 });
-  
